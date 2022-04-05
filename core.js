c9ae989dced0a79c7829a81d6e89ed35226cb6a0
Dominic Sutton <dominic.sutton@dominic.sutton-MBP-Intel-C02DR7K4MD6M>
2022-04-05 10:55:09 +0100

    Updated coding standard

--- core.js
+++ core.js
@@ -1,14 +1,14 @@
-$j('[data-ajax]').click(function(e){
-	e.preventDefault();
-	$elem = $j(this);
-	if(typeof window[$elem.data('ajax')] != "undefined"){
-		var callback = window[$elem.data('ajax')]();
-	}
-	$j.ajax({
-		url: $elem.attr('href')
-	}).done(function(){
-		if(typeof callback == "function"){
-			callback();
-		}
-	})
+jQuery('[data-ajax]').click((e) => {
+    e.preventDefault();
+    $elem = jQuery(this);
+    if(typeof window[$elem.data('ajax')] != "undefined"){
+        var callback = window[$elem.data('ajax')]();
+    }
+    jQuery.ajax({
+        url: $elem.attr('href')
+    }).done(() => {
+        if(typeof callback == "function"){
+            callback();
+        }
+    })
 });
